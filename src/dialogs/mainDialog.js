2fb9320a1629d86efb73c5fc4e69788b680e5a5d
4lvaro4chin <alvaro.achin@outlook.com>
2021-09-07 08:24:06 -0500

    Commit paso 9

--- src/dialogs/mainDialog.js
+++ src/dialogs/mainDialog.js
@@ -1,8 +1,9 @@
-const { ComponentDialog, WaterfallDialog, DialogSet, DialogTurnStatus } = require("botbuilder-dialogs");
+const { ComponentDialog, WaterfallDialog, DialogSet, DialogTurnStatus, ChoicePrompt, ChoiceFactory, ListStyle } = require("botbuilder-dialogs");
 const { MenuInicialDialog } = require("./menuInicialDialog");
 
 const MAIN_DIALOG = 'mainDialog';
 const MENU_INICIAL_DIALOG = 'menuInicialDialog';
+const CHOICE_PROMPT = 'choicePrompt';
 const WATERFALL_DIALOG = 'waterfallDialog';
 
 class MainDialog extends ComponentDialog {
@@ -10,19 +11,43 @@ class MainDialog extends ComponentDialog {
         super(dialogId);
 
         this.addDialog(new WaterfallDialog(WATERFALL_DIALOG, [
-            this.initilStep.bind(this),
+            this.initialStep.bind(this),
+            this.continueStep.bind(this),
             this.finalStep.bind(this)
         ]))
-            .addDialog(new MenuInicialDialog(MENU_INICIAL_DIALOG));
+            .addDialog(new MenuInicialDialog(MENU_INICIAL_DIALOG))
+            .addDialog(new ChoicePrompt(CHOICE_PROMPT));
 
         this.initialDialogId = WATERFALL_DIALOG;
     }
 
-    async initilStep(stepContext) {
+    async initialStep(stepContext) {
         return await stepContext.beginDialog(MENU_INICIAL_DIALOG);
     }
 
+    async continueStep(stepContext) {
+        return await stepContext.prompt(CHOICE_PROMPT, {
+            choices: ChoiceFactory.toChoices(['Si, ver menú.', 'No, gracias.']),
+            prompt: '¿Necesitas que te ayude con algo más?',
+            style: ListStyle.heroCard
+        });
+    }
+
     async finalStep(stepContext) {
+        const menuData = stepContext.options;
+
+        menuData.options = stepContext.result.value;
+
+        switch(menuData.option){
+            case 'Si, ver menú.':
+                console.log('Reiniciar Menú Inicial Dialog');
+                break;
+            case 'No, gracias.':
+                let userName = stepContext.context.activity.from.name;
+                await stepContext.context.sendActivity(`**${ userName }** ha sido un gusto poder ayudarte. Si necesitas algo más, no dudes en contactarme.`);
+                break;
+        }        
+
         console.log('Fin Main Dialog');
         return await stepContext.endDialog();
     }
@@ -35,6 +60,15 @@ class MainDialog extends ComponentDialog {
 
         const dialogContext = await dialogSet.createContext(context);
         const results = await dialogContext.continueDialog();
+
+        switch(context.activity.text){
+            case 'Si, ver menú.':
+                console.log('Intencion continuar Diálogo');
+                newDialog = false;
+                await dialogContext.cancelAllDialogs();
+                await dialogContext.beginDialog(MAIN_DIALOG);
+                break;
+        }        
 
         if (results.status === DialogTurnStatus.empty && newDialog === true) {
             console.log('Nuevo diálogo Main Dialog');
